739abdfd72546e3ab7047a07773ed13eb6f9f4f8
Peter Dalinis <peter@dalinis.com>
2014-03-28 11:27:46 -0700

    messy, but moving forward

---
+++ docs/README.md
@@ -0,0 +1,21 @@
+# Some sort of document
+
+## Overview
+This is a cool document!
+
+### A list
+1. Ruby 1.9.3
+2. Install the redcarpet and xmlrpc gems.
+
+```Some code
+```Some more code!
+
+## test
+[A relative test link](test)
+
+
+| Tables        | Are           | Cool  |
+| ------------- |:-------------:| -----:|
+| col 3 is      | right-aligned | $1600 |
+| col 2 is      | centered      |   $12 |
+| zebra stripes | are neat      |    $1 |
--- stc.rb
+++ stc.rb
@@ -10,8 +10,12 @@ class StashToConfluence
       @token = @confluence.login(user, password)
     end
 
-    def get_page(id)
+    def get_page_by_id(id)
       @confluence.getPage(@token, id.to_s)
+    end
+
+    def get_page_by_space(space, title)
+      @confluence.getPage(@token, space, title)
     end
 
     def save_page(page)
@@ -32,20 +36,45 @@ class StashToConfluence
 
 
   class Application
-    def initialize(user, password)
+    def initialize(user, password, space, app_name)
       @confluence = Confluence.new("", user, password)
       @markdown = MarkdownToHtml.new()
       @header = @markdown.render(File.read('header.md'))
 
+      @space = space
+      @app_name = app_name
 
     end
 
     def go
-      page = @confluence.get_page(42726778)
-      page['content'] = "#{@header}<hr/>#{@markdown.render(File.read('README.md'))}"
-      page['title'] = 'README'
+      docs_dir = 'docs'
+      files = Dir.entries(docs_dir).reject{ |d| d == '.' || d == '..' }.map{ |f| f.sub('.md', '') }
+
+      start_page = files.first{ |f| f.upcase == 'README' }
+
+      page = @confluence.get_page_by_space(@space, @app_name)
+#      page = @confluence.get_page_by_id(42726778)
+      page['content'] = "#{@header}<hr/>#{@markdown.render(File.read("#{docs_dir}/#{start_page}.md"))}"
+      page['title'] = @app_name
 
       @confluence.save_page(page)
+
+      start_page_id = page['id']
+
+
+      files.reject { |f| f == start_page }.each do |f|
+        begin
+          page = @confluence.get_page_by_space(@space, f)
+        rescue
+          page = { "content" => "", "title" => "", "space" => @space, "parentId" => start_page_id }
+        end
+
+        rendered = @markdown.render(File.read("#{docs_dir}/#{f}.md"))
+        page['content'] = "#{@header}<hr/>#{rendered}"
+        page['title'] = f
+
+        @confluence.save_page(page)
+      end
     end
   end
 end
